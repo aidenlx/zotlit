c8405b7e9e632d060e7c4e7fa700d7ed1ddfe409
aidenlx <31102694+aidenlx@users.noreply.github.com>
2026-05-09 18:08:56 +0800

    docs(db): add drizzle.config.js comment about why using .js

--- packages/db/drizzle.config.js
+++ packages/db/drizzle.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
   schema: "./src/schema.ts",
   out: "./drizzle",
   dbCredentials: { url: process.env.ZOTERO_DB_URL },
+  // we use .js config instead of .ts because
+  // drizzle-kit jiti loader have trouble passing nested keys through
+  // to validator like `introspect.casing` here
   introspect: { casing: "preserve" },
 });
